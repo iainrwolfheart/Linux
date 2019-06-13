0dfb019f29f54719a32b986bdc3814b98fa785e4
iainrwolfheart <iainrgorrie@gmail.com>
2019-06-13 12:50:16 +0100

    Linux

---
+++ GitPush.sh
@@ -0,0 +1,15 @@
+stamp='date +%y/%m/%d__%H:%M';
+
+git add .
+
+echo 'Linux Commit ${stamp}'
+read commitMessage
+
+git commit -m "$commitMessage"
+
+echo 'Enter the name of the branch:'
+read branch
+
+git push origin $branch
+
+read
